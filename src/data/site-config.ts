029f0d5301d5cb6dfefa4a8af197e95082d0dff8
Javi2323J <106765529+Javi2323J@users.noreply.github.com>
2025-01-15 12:56:54 +0100

    Change about me description

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -72,7 +72,7 @@ const siteConfig: SiteConfig = {
     socialLinks: [
         {
             text: 'Linkedin',
-            href: 'www.linkedin.com/in/javi2323j'
+            href: 'https://www.linkedin.com/in/javi2323j'
         },
         {
             text: 'GitHub',
@@ -81,10 +81,10 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        text: "Hi, I'm **Javier Romera**, a passionate Web Developer with expertise in the MERN stack (MongoDB, Express.js, React, Node.js). I recently completed an intensive coding bootcamp, where I honed my skills in creating dynamic and user-friendly web applications. Now, I'm eager to bring my knowledge and creativity to a professional team as I pursue new opportunities in the tech industry. Feel free to take a look at my GitHub or contact me!",
         image: {
             src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
+            alt: 'Javier Romera'
         },
         actions: [
             {
